9c8d7e52cbcdbd8e119c2a7f46391ec4c65c93f8
Jens Henneberg <35842959+hjenneberg@users.noreply.github.com>
2024-12-10 07:07:54 +0100

    build: Update development environment (#3)
    * build: Add ddev environment
    
    * build: Add changeset tools
    
    * docs: Add changeset
    
    * style: Adapt code style
    
    * build: Configure dependabot
    
    * build: Add git hook
    
    * build: Add lua formatter config file
    
    * style: Adopt code style
    
    * build: Update ignore files

---
+++ .ddev/homeadditions/.bash_aliases
@@ -0,0 +1,4 @@
+alias ll="ls -lisah --group-directories-first"
+
+alias d="npm run dev"
+alias p="npm run build && npm run start"
--- src/functions/round.lua
+++ src/functions/round.lua
@@ -1,5 +1,6 @@
+---@diagnostic disable-next-line: lowercase-global
 function round(num, numDecimalPlaces)
-    local multi = 10^(numDecimalPlaces or 0)
+    local multi = 10 ^ (numDecimalPlaces or 0)
 
     return math.floor(num * multi + 0.5) / multi
 end
