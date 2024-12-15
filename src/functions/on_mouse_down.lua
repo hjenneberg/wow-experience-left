722faf4196615e990925f1addfb61f2885579e76
Jens Henneberg <hens.jenneberg@gmail.com>
2024-12-15 17:30:42 +0100

    chore: Simplify menu

--- src/functions/on_mouse_down.lua
+++ src/functions/on_mouse_down.lua
@@ -1,35 +1,19 @@
 function CreateMenu(root)
 	root:CreateTitle("ExperienceLeft 0.3.0")
 
-	local submenuSession = root:CreateButton("Session")
 	if IsPaused then
-		submenuSession
-			:CreateButton("Continue session", ContinueSession)
+		root:CreateButton("Continue session", ContinueSession)
 			:SetTitleAndTextTooltip("", "Continue current session. I'm back.")
 	else
-		submenuSession
-			:CreateButton("Pause session", PauseSession)
+		root:CreateButton("Pause session", PauseSession)
 			:SetTitleAndTextTooltip("", "Stop recording session data. I'm AFK or otherwise involved")
 	end
-	submenuSession:CreateDivider()
-	submenuSession
-		:CreateButton("Start new session", ResetSessionXP)
-		:SetTitleAndTextTooltip("", "Attention, this will delete all previously recorded data.")
-
-	--rootDescription:CreateDivider()
-	--local submenu = rootDescription:CreateButton("My Submenu")
-	--submenu:CreateButton("Enable", function()
-	--	return 1
-	--end, true)
-	--submenu:CreateButton("Disable", function()
-	--	return 1
-	--end, false)
+	root:CreateButton("Start new session", ResetSessionXP)
+		:SetTitleAndTextTooltip("", "This will delete all previously recorded data.")
 
 	root:CreateDivider()
 
-	local submenuFrame = root:CreateButton("Frame")
-	submenuFrame
-		:CreateButton("Hide frame", HideFrame)
+	root:CreateButton("Hide frame", HideFrame)
 		:SetTitleAndTextTooltip("", "Hide frame. Use |cFFFF9900/xpleft show|r to show it again.")
 end
 
